559cacb7c3bd0187806e7514ee95217b76204286
Preeti Singh <preetisingh@Preetis-MacBook-Air.local>
2026-03-05 16:00:10 +0530

    product revie system

---
+++ E-comm_Review_System/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { CustomerLayout } from './layout/customer-layout/customer-layout';
+
+export const routes: Routes = [
+
+    { path: 'customer', component: CustomerLayout },
+];

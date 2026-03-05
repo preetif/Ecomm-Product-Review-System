456b78124786e7b01f8b8ba742bf5063945425c1
Preeti Singh <preetisingh@Preetis-MacBook-Air.local>
2026-03-05 17:54:02 +0530

    not working

--- E-comm_Review_System/src/app/app.routes.ts
+++ E-comm_Review_System/src/app/app.routes.ts
@@ -1,7 +1,28 @@
 import { Routes } from '@angular/router';
 import { CustomerLayout } from './layout/customer-layout/customer-layout';
+import { Home } from './customer/home/home';
+import { Product } from './customer/product/product';
+import { Categories } from './customer/categories/categories';
+import { About } from './customer/about/about';
 
 export const routes: Routes = [
+  {
+    path: 'customer',
+    component: CustomerLayout,
+    children: [
+      {
+        path: 'home',
+        component: Home
+      },
+      {path: 'product', component: Product},
+      {path: 'categories', component: Categories},
+      {path: 'about', component: About},
 
-    { path: 'customer', component: CustomerLayout },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      }
+    ]
+  }
 ];
--- E-comm_Review_System/src/app/layout/customer-layout/customer-layout.ts
+++ E-comm_Review_System/src/app/layout/customer-layout/customer-layout.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-customer-layout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './customer-layout.html',
   styleUrl: './customer-layout.css',
 })
